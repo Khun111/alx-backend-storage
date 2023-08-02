7459c10133271a7d51901ec81c7f5fa6dda03e86
Oluwatobi Amure <106190886+Khun111@users.noreply.github.com>
2023-08-02 08:33:31 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- SQL script that lists all bands with Glam rock as their main style
-SELECT band_name, (2022 - formed) lifespan
+SELECT band_name, (IFNULL(split, 2022) - formed) lifespan
 FROM metal_bands
 WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
