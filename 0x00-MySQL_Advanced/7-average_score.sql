91f01ce62eb51524cd259e65b6d9721ec17bf86c
Oluwatobi Amure <106190886+Khun111@users.noreply.github.com>
2023-08-04 19:47:48 +0100

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,7 +3,7 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
   DECLARE avg_score INT
-  SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id
+  SELECT AVG(score) INTO avg_score FROM corrections WHERE corrections.user_id = user_id
   UPDATE users SET average_score = avg_score WHERE id = user_id
 END //
 DELIMITER ;
